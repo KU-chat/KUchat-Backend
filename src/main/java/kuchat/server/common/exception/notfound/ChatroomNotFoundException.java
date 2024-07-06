b4e4f091b7c4fa644af41c9c819ee84040166389
이영선 <leedrkr323@konkuk.ac.kr>
2024-07-06 16:08:30 +0900

    fix: exception 클래스 마저 제거

--- src/main/java/kuchat/server/common/exception/notfound/ChatroomNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package kuchat.server.common.exception.notfound;
-
-public class ChatroomNotFoundException extends NotFoundException{
-    public ChatroomNotFoundException() {
-        super("존재하지 않는 채팅방입니다.", 4001);
-    }
-}
