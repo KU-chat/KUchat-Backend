77d577a0588d0c9fd9583e5c2535b68fa2e9da93
lyouxsun <leedrkr323@konkuk.ac.kr>
2024-09-03 19:25:15 +0900

    fix: 리다이렉트 url 변경2

--- src/main/java/kuchat/server/common/config/SecurityConfig.java
+++ src/main/java/kuchat/server/common/config/SecurityConfig.java
@@ -59,7 +59,7 @@ public class SecurityConfig {
                     @Override
                     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
                         CorsConfiguration configuration = new CorsConfiguration();
-                        configuration.setAllowedOrigins(List.of("https://www.kuchat.site", "http://localhost:9000", "https://localhost:3000"));
+                        configuration.setAllowedOrigins(List.of("https://www.kuchat.site", "http://localhost:9000", "http://localhost:3000"));
                         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                         configuration.setAllowedHeaders(Collections.singletonList("*"));
                         configuration.setMaxAge(3600L);
