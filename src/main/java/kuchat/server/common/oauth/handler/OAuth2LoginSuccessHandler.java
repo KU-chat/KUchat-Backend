aa515115f67b5bf2795d14a88397f5cc11083433
lyouxsun <leedrkr323@konkuk.ac.kr>
2025-01-17 14:02:35 +0900

    fix: redirect url 변경

--- src/main/java/kuchat/server/common/oauth/handler/OAuth2LoginSuccessHandler.java
+++ src/main/java/kuchat/server/common/oauth/handler/OAuth2LoginSuccessHandler.java
@@ -1,6 +1,5 @@
 package kuchat.server.common.oauth.handler;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -9,7 +8,6 @@ import kuchat.server.common.exception.KuchatException;
 import kuchat.server.common.jwt.AuthToken;
 import kuchat.server.common.jwt.JwtTokenService;
 import kuchat.server.common.oauth.CustomOAuth2User;
-import kuchat.server.common.redis.RedisService;
 import kuchat.server.common.response.BaseResponseStatus;
 import kuchat.server.domain.enums.Role;
 import kuchat.server.domain.member.Member;
@@ -20,8 +18,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
-
 @Slf4j
 @RequiredArgsConstructor
 @Service
@@ -30,7 +26,6 @@ public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
     private final JwtTokenService jwtTokenService;
     private final MemberRepository memberRepository;
     private static final String REFRESH_TOKEN = "Authorization-refresh";
-    private final RedisService redisService;
 
     @Transactional
     @Override
@@ -51,10 +46,10 @@ public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
                 log.info("[SuccessHandler] guest token 생성 = {}", guestToken);
 
                 // 프론트와 api 연동 시 redirect url
-//                String url = "http://localhost:3000/signup?guest-token=" + guestToken;
+                String url = "http://localhost:3000/signup?guest-token=" + guestToken;
 
                 // 백에서 테스트 시 redirect url
-                String url = "http://localhost:9000/member/signup?guest-token=" + guestToken;
+//                String url = "http://localhost:9000/member/signup?guest-token=" + guestToken;
 
                 log.info("[redirect url] " + url);
                 response.sendRedirect(url);
@@ -75,9 +70,8 @@ public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
                         authToken.getAccessToken(), authToken.getRefreshToken());
 
                 // 클라이언트와 API 통신을 하는 경우 (develop 브랜치)
-//                response.sendRedirect("http://localhost:3000/user");
+                response.sendRedirect("http://localhost:3000/user");
 
-                // 서버 단에서 템플릿 엔진을 사용하여 구현하는 경우 (local)
                 Cookie cookie = new Cookie("Authorization", authToken.getAccessToken());
                 cookie.setHttpOnly(true); // XSS 공격 방지
                 cookie.setSecure(true);   // HTTPS 에서만 전송 (개발 환경에서는 설정 비활성화 가능)
@@ -85,8 +79,8 @@ public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
                 cookie.setMaxAge(60 * 60); // 쿠키 만료 시간 설정 (1시간)
                 response.addCookie(cookie);
 
-                response.sendRedirect("/");
-
+                // 서버 단에서 템플릿 엔진을 사용하여 구현하는 경우 (local)
+//                response.sendRedirect("/");
             }
         } catch (Exception e) {
             log.error(e.getMessage());
