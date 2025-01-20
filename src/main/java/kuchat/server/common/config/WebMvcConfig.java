dedeb9c5f98337cb27c5ac2dc7d1055fa2de6eca
lyouxsun <leedrkr323@konkuk.ac.kr>
2025-01-20 18:14:49 +0900

    fix: cors 도메인 추가

--- src/main/java/kuchat/server/common/config/WebMvcConfig.java
+++ src/main/java/kuchat/server/common/config/WebMvcConfig.java
@@ -54,7 +54,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         log.info("[addCorsMappings] CorsMapping 호출");
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:9000", "https://www.kuchat.site")
+                .allowedOrigins("https://kuchat.netlify.app", "http://localhost:9000", "https://www.kuchat.site")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH","OPTIONS")
                 .exposedHeaders("location", "Authorization")
                 .allowedHeaders("Content-Type", "Authorization", "X-Requested-With", "Accept")
