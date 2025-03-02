9986d0b0d6a2bd1ac50fb022c8803e6a66924707
lyouxsun <leedrkr323@konkuk.ac.kr>
2025-03-02 22:46:52 +0900

    fix: code 데이터 받는 방식 변경

---
+++ src/main/java/kuchat/server/common/oauth/AuthRequest.java
@@ -0,0 +1,14 @@
+package kuchat.server.common.oauth;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthRequest {
+    private String code;
+}
--- src/main/java/kuchat/server/common/oauth/OAuthController.java
+++ src/main/java/kuchat/server/common/oauth/OAuthController.java
@@ -5,10 +5,7 @@ import kuchat.server.domain.member.dto.AuthTokenResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RequestMapping("/oauth")
@@ -19,10 +16,10 @@ public class OAuthController {
     private final OAuthService oAuthService;
 
     @PostMapping("/google")
-    public ResponseEntity<AuthTokenResponse> callback(@RequestParam("code") String code,
+    public ResponseEntity<AuthTokenResponse> callback(@ModelAttribute AuthRequest authRequest,
                                                       HttpServletResponse response){
-        log.info("[callback] 구글 인가코드 발급 완료 = {}", code);
-        AuthTokenResponse authTokenResponse = oAuthService.process(code, response);
+        log.info("[callback] 구글 인가코드 발급 완료 = {}", authRequest.getCode());
+        AuthTokenResponse authTokenResponse = oAuthService.process(authRequest.getCode(), response);
         return ResponseEntity.ok(authTokenResponse);
     }
 }
